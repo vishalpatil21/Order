89fc9bd5fbe2035fcb3c6be350029217eee81849
root <root@HP-PC>
2024-04-27 00:32:25 -0700

    resillience retry and ratelimit configuration added

--- src/main/java/microservice/registration/service/OrderService.java
+++ src/main/java/microservice/registration/service/OrderService.java
@@ -1,9 +1,12 @@
 package microservice.registration.service;
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import microservice.registration.restclient.InventoryFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import io.github.resilience4j.retry.annotation.Retry;
+import java.util.concurrent.TimeoutException;
 
 @Service
 public class OrderService {
@@ -12,6 +15,8 @@ public class OrderService {
     private InventoryFeignClient inventoryFeignClient;
 
     @CircuitBreaker(name = "inventoryHealth", fallbackMethod = "fallbackHealth")
+    @Retry(name = "inventoryRetry")
+    @RateLimiter(name = "inventoryRateLimiter")
     public String getInventoryHealth() {
         return inventoryFeignClient.getInventoryHealth();
     }
